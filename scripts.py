842626348659221e476b2575cd3993b9e1b355ad
王璐雅 <wangluya@bytedance.com>
2020-10-03 11:45:12 +0800

    second push

--- scripts.py
+++ scripts.py
@@ -1,36 +1,45 @@
+import json
 import os
 import time
 
 import xlwt
 
+#
+# def redis_nil():
+#     f=open("0000",'r')
+#     lines=f.readlines()
+#     wf=open('00','w')
+#     for line in lines:
+#         # j=json.dumps(line)
+#         # vid=json.load(line)
+#         # print(vid["vid"])
+#
+#         l=line.split("\"vid\": \"")
+#         for item in l[1:]:
+#             try:
+#                 vid = item.split('\"')[0]+'\n'
+#                 # print(vid)
+#                 wf.write(vid)
+#
+#             except:
+#                 pass
+#     f.close()
+#     wf.close()
 
-def redis_nil():
-    f=open("0000",'r')
-    lines=f.readlines()
-    wf=open('00','w')
-    for line in lines:
-        # j=json.dumps(line)
-        # vid=json.load(line)
-        # print(vid["vid"])
 
-        l=line.split("\"vid\": \"")
-        for item in l[1:]:
-            try:
-                vid = item.split('\"')[0]+'\n'
-                # print(vid)
-                wf.write(vid)
+def zero():
+    f=open('0000')
 
-            except:
-                pass
-    f.close()
-    wf.close()
-
-
-
+    #just one line
+    line=f.readline()
+    js_line=json.loads(line)
+    print(js_line)
+    #success
 
 
 
 
 
 if __name__ == '__main__':
-    redis_nil()
+    # redis_nil()
+    zero()
